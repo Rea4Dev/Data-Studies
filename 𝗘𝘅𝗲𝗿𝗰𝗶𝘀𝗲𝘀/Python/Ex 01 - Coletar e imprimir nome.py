abc8038f8f1b930c837b6cc298b6d2b722c5848a
Rea4Dev <rea4personal@outlook.com>
2024-04-26 21:58:16 -0300

    att

---
+++ 𝗘𝘅𝗲𝗿𝗰𝗶𝘀𝗲𝘀/Python/Ex 01 - Coletar e imprimir nome.py
@@ -0,0 +1,37 @@
+#Crie um script Python que leia o nome de uma pessoa e mostra uma mensagem de boas-vindas de acordo com o valor digitado.
+
+
+#? Forma 01
+
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+
+
+
+#? Forma 02
+
+print("Digite seu nome: ")
+nome = input()
+print(f"Olá, {nome}!")
+
+
+
+
+#? Forma 03
+
+print("Digite seu nome: ")
+nome = input()
+print("Olá, " + nome + "!")
+
+
+
+#? Forma 04
+
+print("Digite seu nome: ")
+nome = input()
+print("Olá, ", end="")
+print(nome, end="")
+print("!")
+
+#Para que o print() não quebre linha após ser usado, é necessário usar este end="". Ele finalizará a linha apenas com o que tem escrito no "", que no caso, é nada.
