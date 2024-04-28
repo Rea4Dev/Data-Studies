7c5ea8a1fa4ef43145cfc74b5806559018b27932
Rea4Dev <rea4personal@outlook.com>
2024-04-28 17:34:58 -0300

    att

--- 𝗘𝘅𝗲𝗿𝗰𝗶𝘀𝗲𝘀/Python/Ex 04 - Coletar e imprimir soma.py
+++
@@ -1,6 +0,0 @@
-#Crie um script Python que leia dois números e tente mostrar a soma entre eles.
-
-numero1 = input("Digite o primeiro número: ")
-numero2 = input("Digite o segundo número: ")
-
-print(f"A soma entre {numero1} e {numero2} é {int(numero1)+int(numero2)}")
