16e8ecbf83ca343f004b3b1ac79894af3a9e3ef5
Rea4Dev <rea4personal@outlook.com>
2024-04-29 12:43:54 -0300

    att

---
+++ 𝗘𝘅𝗲𝗿𝗰𝗶𝘀𝗲𝘀/Python/Ex 08 - Metros para centímetros e milímetros.py
@@ -0,0 +1,4 @@
+# Desenvolva um programa que leia as duas notas de um aluno. Calcule e mostre a sua média.
+
+metros = float(input("Digite em metros o valor a ser convertido para decímetros, centímetros e milímetros: "))
+print(f"{metros:.0f}M ➡ {metros*10:.0f}dm ➡ {metros*100:.0f}cm ➡ {metros*1000:.0f}mm.") 
