32aeb61a8d383529e6a34d467a69a03550192897
Rea4Dev <rea4personal@outlook.com>
2024-05-09 18:03:03 -0300

    att

--- 𝗘𝘅𝗲𝗿𝗰𝗶𝘀𝗲𝘀/Python/Manipulando textos/Ex 23 - Primeira e última ocorrência de uma string.py
+++ 𝗘𝘅𝗲𝗿𝗰𝗶𝘀𝗲𝘀/Python/Manipulando textos/Ex 23 - Primeira e última ocorrência de uma string.py
@@ -1,7 +1,7 @@
 
-#? Faça um programa que leia uma frase pelo teclado e mostre quantas vezes apareceu a letra "A", em que posição ela aparece a primeira e a última vez.
+#! Faça um programa que leia uma frase pelo teclado e mostre quantas vezes apareceu a letra "A", em que posição ela aparece a primeira e a última vez.
 
-nome = input('Digite o nome: ')
+nome = input('Digite o nome: ').lower().strip()
 
 print(f'A letra A aparece {nome.count('a')} vezes na frase.')
 print(f'A primeira letra A apareceu na posição de índice: {nome.lower().find('a')}')
